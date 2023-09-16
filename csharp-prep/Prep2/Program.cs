78b52cb3d0b96e5c3643136bb70079156a08ce9b
benjaminihentuge <osondubi@byui.edu>
2023-09-16 16:28:56 +0100

    latest

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your Grade percentage? ");
+        string value = Console.ReadLine();
+       
+        int gradePercentage = int.Parse(value);
+        string letter = "";
+
+        if (gradePercentage >= 90 )
+        {   
+            string score = "";
+            letter = "A";
+            int ff = gradePercentage % 10;
+            if (ff>7)
+            {
+                score = "+";
+            }
+            else if (ff<3)
+            {
+                score = "-";
+            }
+            Console.Write($"You got an {letter}{score} Congratulations you passed the class");
+        }
+        
+        else if (gradePercentage >= 80)
+        {   
+            string score = "";
+            letter = "B";
+            int ff = gradePercentage % 10;
+            if (ff>7)
+            {
+                score = "+";
+            }
+            else if (ff<3)
+            {
+                score = "-";
+            }
+            Console.Write($"You got a {letter}{score} Congratulations you passed the class");
+        }
+        
+        else if (gradePercentage >= 70)
+        {   
+            string score = "";
+            letter = "C";
+            int ff = gradePercentage % 10;
+            if (ff>7)
+            {
+                score = "+";
+            }
+            else if (ff<3)
+            {
+                score = "-";
+            }
+            Console.Write($"You got a {letter}{score} Congratulations you passed the class");
+        }
+        
+
+        else if (gradePercentage >= 60)
+        {   
+            letter = "D";
+            Console.Write($"You got a {letter}. Better luck next semester.");
+        }
+
+        else if (gradePercentage < 60)
+        {
+
+            letter = "F";
+            Console.Write($"You got an {letter}. Try again next semester.");
+        }
     }
+
 }
