ae87127a91f13186943677041f71fe68f9f48b6e
benjaminihentuge <osondubi@byui.edu>
2023-09-21 23:07:36 +0100

    21-09-2023

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {  
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 20);
+        int guess = 0;
+        int counts = 0;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            counts++;
+        }
+
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"It took you {counts} attempts.");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int guess = -1;
+
+        while (guess != 0)
+        {
+            Console.Write("Write a number? ");
+            guess = int.Parse(Console.ReadLine());
+            numbers.Add(guess);
+        }
+        Console.WriteLine("Finoto");
+        foreach (int number in numbers)
+        {
+            if (number != 0)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        
+        int total = 0;
+        int count = 0;
+
+        count = numbers.Count;
+        foreach (int value in numbers)
+        {
+            total += value;
+        }
+        int average = total / count;
+
+        int max = int.MinValue;
+        foreach (int digit in numbers)
+        {
+            if (digit > max)
+            {
+                max = digit;
+            }
+        }
+        Console.WriteLine($"The total sum is {total}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {max}");
+        int smallestPositive = int.MaxValue; 
+
+        foreach (int digit in numbers)
+        {
+            if (digit > 0 && digit < smallestPositive)
+            {
+                smallestPositive = digit;
+            }
+        }
+
+        if (smallestPositive != int.MaxValue)
+        {
+            Console.WriteLine($"The smallest positive number is {smallestPositive}");
+        }
+        else
+        {
+            Console.WriteLine("No positive numbers were entered.");
+        }
+
+        
     }
 }
