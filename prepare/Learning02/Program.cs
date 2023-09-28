b7527d0b07499fe6f406dbe4f1b8c13673ad940b
benjaminihentuge <osondubi@byui.edu>
2023-09-28 11:05:47 +0100

    28-09-2023

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Fresh Dew";
+        job1._jobTitle = "Cashier";
+        job1._startYear = 2017;
+        job1._endYear = 2017;
+        
+
+        Job job2 = new Job();
+        job2._company = "FCDA";
+        job2._jobTitle = "Engineer";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+        
+        Resume myResume = new Resume();
+        myResume._name = "Osondu Benjamin Ihentuge";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
     }
 }
